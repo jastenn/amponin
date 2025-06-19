b94f2b27da5d515da1a14d4596ad7ab1681cb95b
Justine Aviso <justine.aviso001@gmail.com>
2025-06-19 18:42:05 +0800

    cascade delete added on accounts tables

--- migrations/000003_create_local_account.up.sql
+++ migrations/000003_create_local_account.up.sql
@@ -1,6 +1,5 @@
 create table if not exists local_accounts (
-    local_account_id text primary key default nanoid(8),
-    user_id text not null references users(user_id),
+    user_id text not null references users(user_id) on delete cascade,
     password_hash bytea not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now()
--- migrations/000004_create_foreign_account.up.sql
+++ migrations/000004_create_foreign_account.up.sql
@@ -1,5 +1,5 @@
 create table if not exists foreign_accounts(
-    user_id text not null references users(user_id),
+    user_id text not null references users(user_id) on delete cascade,
     provider text not null, -- provider name, e.g. google or facebook
     provider_id text not null, -- assigned id of the account by the provider
     created_at timestamptz not null default now(),
